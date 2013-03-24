5001c7d8afbf362069936326a62c0923f45ba185
Michael 'devbug' Williams <m.t.williams@live.com>
2013-03-24 15:58:46 -0700

    Defined bt::RenderTarget interface.

--- code/include/bt/render_target.h
+++ code/include/bt/render_target.h
@@ -7,8 +7,39 @@
 #include <bt/foundation.h>
 #include <bt/config.h>
 
+#include <bt/texture.h>
+
 namespace bt {
   class RenderTarget {
+    protected:
+      RenderTarget();
+      ~RenderTarget();
+
+    public:
+      static RenderTarget* create(
+        PixelFormat format,
+        unsigned width,
+        unsigned height );
+
+      void destroy();
+
+      void resize(
+        unsigned width,
+        unsigned height );
+
+    public:
+      FOUNDATION_INLINE PixelFormat format() const
+      { return _texture->format(); }
+
+      FOUNDATION_INLINE void dimensions( 
+        unsigned& width,
+        unsigned& height ) const
+      {
+        _texture->dimensions(width, height);
+      }
+
+    protected:
+      Texture* _texture;
   };
 } // bt
 
