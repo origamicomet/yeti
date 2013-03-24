088fe19803fafc9276c77264e0f480d60d0f5947
Michael 'devbug' Williams <m.t.williams@live.com>
2013-03-24 16:58:34 -0700

    Defined bt::Resource interface.

--- code/include/bt/resource.h
+++ code/include/bt/resource.h
@@ -9,6 +9,45 @@
 
 namespace bt {
   class Resource {
+    public:
+      typedef uint32_t Id;
+      static const Id invalid_id = 0xFFFFFFFFu;
+
+      struct CompileData {
+        void (*log)( const char* format, ... );
+        const char *data, *data_src, *path;
+        FILE *in, *mem_resident_data, *streaming_data;
+        bool debuggable;
+        bool optimized;
+      };
+
+      struct Type {
+        uint32_t id;
+        String assoc_ext;
+        Array<String> assoc_src_ext;
+        Resource* (*load)( ResourceStream* stream );
+        Resource* (*reload)( Resource* resource );
+        void (*unload)( Resource* resource );
+        bool (*compile)( CompileData& compile_data );
+      };
+
+    protected:
+      Resource( Type& type, Id id );
+      virtual ~Resource();
+
+    public:
+      Resource* reload();
+
+    public:
+      FOUNDATION_INLINE Type& type() const
+      { return _type; }
+
+      FOUNDATION_INLINE Id& id() const
+      { return _id; }
+
+    private:
+      Type& _type;
+      Id _id;
   };
 } // bt
 
