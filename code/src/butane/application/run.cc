93c7c2b0eeb481d74d2ec4017ffd548a50bcbc36
Michael 'devbug' Williams <m.t.williams@live.com>
2013-05-19 21:17:46 -0700

    Added basic sjson test in Application::run.

--- code/src/butane/application/run.cc
+++ code/src/butane/application/run.cc
@@ -17,6 +17,18 @@ namespace Application {
     log("Hello, World!\n");
     static const char code[] = "log('Hello, World!\\n')\n--Application.quit()";
     script.load("example", &code[0], sizeof(code) - 1);
+    static const char sjson[] = "{ foo = \"bar\" fizz = \"buzz\" }";
+    static const uint32_t blob_len = 4096;
+    void* blob = (void*)Allocators::heap().alloc(blob_len);
+    const sjson::Object* root;
+    if (!(root = sjson::parse(Allocators::heap(), &sjson[0], blob, blob_len)))
+      log("Failed to parse sjson!\n");
+    else {
+      const sjson::String* foo = (const sjson::String*)root->find("foo");
+      log("foo = '%s'\n", foo->raw());
+      const sjson::String* fizz = (const sjson::String*)root->find("fizz");
+      log("fizz = '%s'\n", fizz->raw());
+    }
   }
 } // Application
 } // butane
