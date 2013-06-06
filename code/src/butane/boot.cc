47733cdc4090d85d10966ac28a06737ae1800521
Michael 'devbug' Williams <m.t.williams@live.com>
2013-06-05 19:59:48 -0700

    Explictly seting locale now (to en-US.UTF-8).

--- code/src/butane/boot.cc
+++ code/src/butane/boot.cc
@@ -3,8 +3,11 @@
 
 #include <butane/application.h>
 
+#include <locale.h>
+
 int main( int argc, const char* argv[] ) {
-  foundation::LogScope _("Engine");
+  const foundation::LogScope _("Engine");
+  setlocale(LC_ALL, "en-US.UTF8");
   butane::Application::boot(argc, &argv[0]);
   return EXIT_FAILURE;
 }
