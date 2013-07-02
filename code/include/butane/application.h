4f01a7310d68e390428bc241d35567cedd22d3ac
Michael 'devbug' Williams <m.t.williams@live.com>
2013-07-02 02:40:37 -0700

    Reordered Application::run forward declarations.

--- code/include/butane/application.h
+++ code/include/butane/application.h
@@ -8,9 +8,9 @@
 
 namespace butane {
   class RenderDevice;
+  class Console;
   class Window;
   class SwapChain;
-  class Console;
 namespace Application {
   extern BUTANE_EXPORT butane::RenderDevice* render_device();
   extern BUTANE_EXPORT void set_render_device(
