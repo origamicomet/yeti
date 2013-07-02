cc81489598ab8dfebd269859000277f669cab5e9
Michael 'devbug' Williams <m.t.williams@live.com>
2013-07-02 02:35:41 -0700

    Alphabetized Resources::expose.

--- code/src/butane/resources.cc
+++ code/src/butane/resources.cc
@@ -4,22 +4,23 @@
 #include <butane/resources.h>
 #include <butane/resources/config.h>
 #include <butane/resources/mesh.h>
+#include <butane/graphics/pixel_shader.h>
 #include <butane/resources/shader.h>
 #include <butane/resources/state.h>
 #include <butane/resources/texture.h>
+#include <butane/resources/unit.h>
 #include <butane/graphics/vertex_shader.h>
-#include <butane/graphics/pixel_shader.h>
 
 namespace butane {
 namespace Resources {
   void expose() {
     ConfigResource::type().expose();
     MeshResource::type().expose();
+    PixelShader::type().expose();
     ShaderResource::type().expose();
     StateResource::type().expose();
     TextureResource::type().expose();
     VertexShader::type().expose();
-    PixelShader::type().expose();
   }
 } // Resources
 } // butane
