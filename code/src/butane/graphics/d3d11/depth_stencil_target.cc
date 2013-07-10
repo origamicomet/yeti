b9b14193479fbb9a795a278e4bef12682da22d6e
Michael 'devbug' Williams <m.t.williams@live.com>
2013-07-10 16:21:01 -0700

    Added DepthStencilTarget::create(texture), see issue #14.

--- code/src/butane/graphics/d3d11/depth_stencil_target.cc
+++ code/src/butane/graphics/d3d11/depth_stencil_target.cc
@@ -69,6 +69,41 @@ namespace butane {
     return depth_stencil_target;
   }
 
+  DepthStencilTarget* DepthStencilTarget::create(
+    Texture* texture )
+  {
+    assert(texture != nullptr);
+
+    if (texture->type() != Texture::TEXTURE_2D)
+      fail("Invalid texture (invalid type, expected a 2D texture).");
+
+    if (!(texture->pixel_format() & (PixelFormat::DepthBit | PixelFormat::StencilBit)))
+      fail("Invalid texture (invalid pixel format, expected a depth and/or stencil format).");
+
+    D3D11DepthStencilTarget* depth_stencil_target =
+      make_new(D3D11DepthStencilTarget, allocator())(texture);
+
+    D3D11_DEPTH_STENCIL_VIEW_DESC dsvd;
+    dsvd.Format             = dxgi_format_from_pixel_format(texture->pixel_format());
+    dsvd.ViewDimension      = D3D11_DSV_DIMENSION_TEXTURE2D;
+    dsvd.Flags = 0;
+    dsvd.Texture2D.MipSlice = 0;
+
+    D3D11Texture* texture_ = (D3D11Texture*)texture;
+
+    D3D11RenderDevice* render_device =
+      ((D3D11RenderDevice*)Application::render_device());
+
+    /* depth_stencil_target->_view = */ {
+      const HRESULT hr = render_device->device()->CreateDepthStencilView(
+        texture_->resource(), &dsvd, &depth_stencil_target->_view);
+      if (FAILED(hr))
+        fail("ID3D11Device::CreateDepthStencilView failed, hr=%#08x", hr);
+    }
+
+    return depth_stencil_target;
+  }
+
   void D3D11DepthStencilTarget::destroy()
   {
     make_delete(D3D11DepthStencilTarget, allocator(), this);
