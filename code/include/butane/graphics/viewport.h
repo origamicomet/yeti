a5313acc0408f9d1397e7da5ed67182e595fd296
Michael 'devbug' Williams <m.t.williams@live.com>
2013-07-29 21:26:16 -0700

    Added Viewport::update().

--- code/include/butane/graphics/viewport.h
+++ code/include/butane/graphics/viewport.h
@@ -54,6 +54,16 @@ namespace butane {
       FOUNDATION_INLINE uint32_t right() const
       { return _right; }
 
+      FOUNDATION_INLINE void update(
+        const uint32_t top,
+        const uint32_t left,
+        const uint32_t bottom,
+        const uint32_t right )
+      {
+        _top = top; _left = left;
+        _bottom = bottom; _right = _right;
+      }
+
     private:
       uint32_t _top, _left;
       uint32_t _bottom, _right;
