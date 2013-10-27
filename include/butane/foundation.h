d65db33d51b9d68062a7faa2e6d77108f2c00857
Michael 'devbug' Williams <m.t.williams@live.com>
2013-10-27 02:24:48 -0700

    Added include/butane/foundation.h.

---
+++ include/butane/foundation.h
@@ -0,0 +1,38 @@
+/* ========================================================================== */
+/*                                                                            */
+/* This file is part of Butane.                                               */
+/*                                                                            */
+/* Author(s):                                                                 */
+/*                                                                            */
+/*   Michael Williams <devbug@bitbyte.ca>                                     */
+/*                                                                            */
+/* See LICENSE.md for licensing information.                                  */
+/*                                                                            */
+/* ========================================================================== */
+/*! @file include/butane/foundation.h
+     Includes everything in foundation and aliases.                           */
+/* ========================================================================== */
+
+#ifndef _BUTANE_FOUNDATION_H_
+#define _BUTANE_FOUNDATION_H_
+
+#include <butane/assert.h>
+#include <foundation/allocator.h>
+#include <foundation/architecture.h>
+#include <foundation/array.h>
+#include <foundation/assert.h>
+#include <foundation/compat/inttypes.h>
+#include <foundation/compat/malloc.h>
+#include <foundation/compat/stdalign.h>
+#include <foundation/compat/stdbool.h>
+#include <foundation/compat/stdint.h>
+#include <foundation/compiler.h>
+#include <foundation/config.h>
+#include <foundation/detect/architecture.h>
+#include <foundation/detect/compiler.h>
+#include <foundation/detect/platform.h>
+#include <foundation/platform.h>
+#include <foundation/preprocessor.h>
+
+
+#endif /* _BUTANE_FOUNDATION_H_ */
