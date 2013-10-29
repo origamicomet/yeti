2f4f173a43c057c8ff78132ed0b59b92ee89f47e
Michael 'devbug' Williams <m.t.williams@live.com>
2013-10-28 17:14:43 -0700

    Added butane_task_t and butane::Task (stubbed).

---
+++ include/butane/task.h
@@ -0,0 +1,122 @@
+/* ========================================================================== */
+/*                                                                            */
+/* This file is part of Butane.                                               */
+/*                                                                            */
+/* Author(s):                                                                 */
+/*                                                                            */
+/*   Michael Williams <devbug@bitbyte.ca>                                     */
+/*                                                                            */
+/* See LICENSE.md for licensing information.                                  */
+/*                                                                            */
+/* ========================================================================== */
+/*! @file include/butane/task.h
+     Provides `butane_task_t` and `butane::Task` which represent a single
+     unit of work that can be scheduled on the CPU.                           */
+/* ========================================================================== */
+
+#ifndef _BUTANE_TASK_H_
+#define _BUTANE_TASK_H_
+
+#include <butane/config.h>
+#include <butane/foundation.h>
+
+/* ========================================================================== */
+/*  Task:                                                                     */
+/*   * C (butane_task_t);                                                     */
+/*   * "C++" (butane::Task).                                                  */
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*  C (butane_task_t):                                                        */
+/* ========================================================================== */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*! */
+typedef uint32_t butane_task_id_t;
+
+/*! */
+typedef uint32_t butane_task_affinity_t;
+
+/*! */
+typedef void (*butane_task_kernel_fn)(void);
+
+/*! */
+typedef struct butane_task {
+  butane_task_id_t id_;
+  butane_task_affinity_t affinity_;
+  butane_task_id_t parent_;
+  butane_task_id_t depends_on_;
+  butane_task_kernel_fn kernel_;
+  size_t num_of_args_;
+  uintptr_t args_[4];
+  size_t refs_;
+} butane_task_t;
+
+/* ========================================================================== */
+
+/*! */
+extern void butane_task_run(const butane_task_t *task);
+
+#ifdef __cplusplus
+}
+#endif
+
+/* ========================================================================== */
+/*  C++ (butane::Task):                                                       */
+/* ========================================================================== */
+
+#ifdef __cplusplus
+namespace butane {
+  /*! @copydoc butane_task_t */
+  class Task : private butane_task_t {
+    public:
+      typedef butane_task_id_t Id;
+      static const Id invalid = ~((Id)0);
+      typedef butane_task_affinity_t Affinity;
+      static const Affinity any = ~((Affinity)0);
+      typedef butane_task_kernel_fn Kernel;
+    public:
+      Task();
+      ~Task();
+    public:
+      void run() const;
+    public:
+      Id id() const {
+        return ((Id)id_); }
+      void set_id(const Id id) {
+        id_ = ((butane_task_id_t)id); }
+      Affinity affinity() const {
+        return ((Affinity)affinity_); }
+      void set_affinity(const Affinity affinity) {
+        affinity_ = ((butane_task_id_t)affinity); }
+      Id parent() const {
+        return ((Id)parent_); }
+      void set_parent(const Id parent) {
+        parent_ = ((butane_task_id_t)parent); }
+      Id depends_on() const {
+        return ((Id)depends_on_); }
+      void set_depends_on(const Id depends_on) {
+        depends_on_ = ((butane_task_id_t)depends_on); }
+      Kernel kernel() const { return ((Kernel)kernel_); }
+      void set_kernel(Kernel kernel) {
+        kernel_ = ((Kernel)kernel); num_of_args_ = 0; }
+      void set_kernel(Kernel kernel, uintptr_t _0) {
+        kernel_ = ((Kernel)kernel);  num_of_args_ = 1;
+        args_[0] = _0; }
+      void set_kernel(Kernel kernel, uintptr_t _0, uintptr_t _1) {
+        kernel_ = ((Kernel)kernel); num_of_args_ = 2;
+        args_[0] = _0; args_[1] = _1; }
+      void set_kernel(Kernel kernel, uintptr_t _0, uintptr_t _1, uintptr_t _2) {
+        kernel_ = ((Kernel)kernel); num_of_args_ = 3;
+        args_[0] = _0; args_[1] = _1; args_[2] = _2; }
+      void set_kernel(Kernel kernel, uintptr_t _0, uintptr_t _1, uintptr_t _2, uintptr_t _3) {
+        kernel_ = ((Kernel)kernel); num_of_args_ = 4;
+        args_[0] = _0; args_[1] = _1; args_[2] = _2; args_[3] = _3; }
+  };
+} /* butane */
+#endif
+
+#endif /* _BUTANE_TASK_H_ */
---
+++ src/butane/task.c
@@ -0,0 +1,84 @@
+/* ========================================================================== */
+/*                                                                            */
+/* This file is part of Butane.                                               */
+/*                                                                            */
+/* Author(s):                                                                 */
+/*                                                                            */
+/*   Michael Williams <devbug@bitbyte.ca>                                     */
+/*                                                                            */
+/* See LICENSE.md for licensing information.                                  */
+/*                                                                            */
+/* ========================================================================== */
+ #  include <butane/task.h>
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*  C (butane_task_t):                                                        */
+/* ========================================================================== */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*butane_task_kernel_0)(void);
+typedef void (*butane_task_kernel_1)(uintptr_t);
+typedef void (*butane_task_kernel_2)(uintptr_t,uintptr_t);
+typedef void (*butane_task_kernel_3)(uintptr_t,uintptr_t,uintptr_t);
+typedef void (*butane_task_kernel_4)(uintptr_t,uintptr_t,uintptr_t,uintptr_t);
+
+void butane_task_run(const butane_task_t *task) {
+  bt_assert(debug, task != NULL);
+  bt_assert(debug, task->refs_ == 1);
+  bt_assert(paranoid, task->kernel_ != NULL);
+  bt_assert(paranoid, task->num_of_args_ > 0);
+  bt_assert(paranoid, task->num_of_args_ <= 4);
+  switch (task->num_of_args_) {
+    case 0:
+      ((butane_task_kernel_0)task->kernel_)();
+      break;
+    case 1:
+      ((butane_task_kernel_1)task->kernel_)(
+        task->args_[0]);
+      break;
+    case 2:
+      ((butane_task_kernel_2)task->kernel_)(
+        task->args_[0],
+        task->args_[1]);
+      break;
+    case 3:
+      ((butane_task_kernel_3)task->kernel_)(
+        task->args_[0],
+        task->args_[1],
+        task->args_[2]);
+      break;
+    case 4:
+      ((butane_task_kernel_4)task->kernel_)(
+        task->args_[0],
+        task->args_[1],
+        task->args_[2],
+        task->args_[3]);
+      break;
+  }
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/* ========================================================================== */
+/*  C++ (butane::Task):                                                       */
+/* ========================================================================== */
+
+#ifdef __cplusplus
+namespace butane {
+  Task::Task() {
+  }
+
+  Task::~Task() {
+  }
+
+  void Task::run() const {
+    butane_task_run((const butane_task_t *)this);
+  }
+} /* butane */
+#endif
