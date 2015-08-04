e38d32afe20333b3ea50299dea170c91f9ec95e4
Michael Williams <m.t.williams@live.com>
2015-08-03 18:43:41 -0700

    Make frustrations caused by Microsoft more apparent.

--- src/yeti.cc
+++ src/yeti.cc
@@ -16,8 +16,8 @@ YETI_BEGIN_EXTERN_C // {
 YETI_PUBLIC
 void __yeti__(void)
 {
-  // HACK(mtwilliams): Export at least one symbol. This is to make sure
-  // Microsoft's linker actually produces a dynamically-linked library.
+  // HACK(mtwilliams): Export at least one symbol. This is to make sure shitty
+  // linkers, like Microsoft's, actually produce a dynamically-linked library.
 }
 
 YETI_END_EXTERN_C // }
