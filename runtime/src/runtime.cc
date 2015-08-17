8d511a9e63f8c20b686000ef712dd293c58801ec
Michael Williams <m.t.williams@live.com>
2015-08-16 22:07:54 -0700

    Stub 'runtime'.

---
+++ runtime/src/runtime.cc
@@ -0,0 +1,19 @@
+//===-- runtime.cc ----------------------------------------*- mode: C++ -*-===//
+//
+//                             __ __     _   _
+//                            |  |  |___| |_|_|
+//                            |_   _| -_|  _| |
+//                              |_| |___|_| |_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "yeti.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+  return EXIT_SUCCESS;
+}
