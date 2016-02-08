dda30208cc234f193a2998ab2cd73a2aa7d05c98
Michael Williams <m.t.williams@live.com>
2016-02-07 16:47:03 -0800

    Provide a (mostly) platform independent keyboard singleton.

---
+++ include/yeti/input/keyboard.h
@@ -0,0 +1,63 @@
+//===-- yeti/input/keyboard.h -----------------------------*- mode: C++ -*-===//
+//
+//                             __ __     _   _
+//                            |  |  |___| |_|_|
+//                            |_   _| -_|  _| |
+//                              |_| |___|_| |_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+//
+// TODO(mtwilliams): Document the purpose of this file.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _YETI_KEYBOARD_H_
+#define _YETI_KEYBOARD_H_
+
+#include "yeti/config.h"
+#include "yeti/linkage.h"
+#include "yeti/foundation.h"
+
+namespace yeti {
+
+namespace Keys {
+  enum _ {
+    #define _(Id, Mnemonic, _) Mnemonic = Id,
+      #include "yeti/input/keys.inl"
+    #undef _
+
+    _COUNT
+  };
+} typedef Keys::_ Key;
+
+namespace Keyboard {
+
+/// Returns wether a keyboard is connected and functioning.
+extern YETI_PUBLIC bool connected();
+extern YETI_PUBLIC bool disconnected();
+
+/// Returns wether the @key was pressed this frame.
+extern YETI_PUBLIC bool pressed(const Key key);
+
+/// Returns wether the @key has been held down for more than one frame.
+extern YETI_PUBLIC bool held(const Key key);
+
+/// Returns wether the @key was released this frame.
+extern YETI_PUBLIC bool released(const Key key);
+
+/// \internal Marks the @key as released for this frame.
+extern YETI_LOCAL void up(const Key key);
+
+/// \internal Marks the @key as pressed for this frame.
+extern YETI_LOCAL void down(const Key key);
+
+/// \internal
+extern YETI_LOCAL void update();
+
+} // Keyboard
+
+} // yeti
+
+#endif // _YETI_KEYBOARD_H_
---
+++ src/yeti/input/keyboard.cc
@@ -0,0 +1,85 @@
+//===-- yeti/input/keyboard.cc ----------------------------*- mode: C++ -*-===//
+//
+//                             __ __     _   _
+//                            |  |  |___| |_|_|
+//                            |_   _| -_|  _| |
+//                              |_| |___|_| |_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "yeti/input/keyboard.h"
+
+namespace yeti {
+
+namespace {
+  // This is essentially a rolling buffer (of bits) that tracks the state of
+  // each key up to 31 frames ago. This appears needlessly complex, but will
+  // allow us to track keys being pushed and released in a single frame.
+  static u32 _state[Keys::_COUNT] = { 0, };
+}
+
+bool Keyboard::connected() {
+  // TODO(mtwilliams): Check if a keyboard is connected.
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  // Refer to http://stackoverflow.com/questions/9930958.
+  // As well as http://stackoverflow.com/questions/6881114.
+  return true;
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+  return true;
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+  return true;
+#elif YETI_PLATFORM == YETI_PLATFORM_IOS ||
+      YETI_PLATFORM == YETI_PLATFORM_ANDROID
+  // HACK(mtwilliams): Assume no hardware keyboard is connected.
+  return false;
+#endif
+}
+
+bool Keyboard::disconnected() {
+  return !connected();
+}
+
+bool Keyboard::pressed(const Key key) {
+  yeti_assert_debug(key > Keys::UNKNOWN);
+  yeti_assert_debug(key < Keys::_COUNT);
+  const u32 prev_state = (_state[key] >> 1) & 1;
+  const u32 state = _state[key] & 1;
+  return (prev_state ^ state) & state;
+}
+
+bool Keyboard::held(const Key key) {
+  yeti_assert_debug(key > Keys::UNKNOWN);
+  yeti_assert_debug(key < Keys::_COUNT);
+  const u32 prev_state = (_state[key] >> 1) & 1;
+  const u32 state = _state[key] & 1;
+  return (prev_state & state) & state;
+}
+
+bool Keyboard::released(const Key key) {
+  yeti_assert_debug(key > Keys::UNKNOWN);
+  yeti_assert_debug(key < Keys::_COUNT);
+  const u32 prev_state = (_state[key] >> 1) & 1;
+  const u32 state = _state[key] & 1;
+  return (prev_state ^ state) & ~state;
+}
+
+void Keyboard::up(const Key key) {
+  yeti_assert_debug(key > Keys::UNKNOWN);
+  yeti_assert_debug(key < Keys::_COUNT);
+  _state[key] &= ~1;
+}
+
+void Keyboard::down(const Key key) {
+  yeti_assert_debug(key > Keys::UNKNOWN);
+  yeti_assert_debug(key < Keys::_COUNT);
+  _state[key] |= 1;
+}
+
+void Keyboard::update() {
+  for (u32 key = 0; key < Keys::_COUNT; ++key)
+    _state[key] = (_state[key] << 1) | (_state[key] & 1);
+}
+
+} // yeti
