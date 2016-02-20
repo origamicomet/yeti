94513ab9ee5bf36e504711874c05ad099b9ecb1f
Michael Williams <m.t.williams@live.com>
2016-02-20 03:14:31 -0800

    Time-step policies.
    Still need to implement TimeStepPolicy::SMOOTHED.

---
+++ include/yeti/time_step_policy.h
@@ -0,0 +1,113 @@
+//===-- yeti/time_step_policy.h ---------------------------*- mode: C++ -*-===//
+//
+//                             __ __     _   _
+//                            |  |  |___| |_|_|
+//                            |_   _| -_|  _| |
+//                              |_| |___|_| |_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+//
+// TODO(mtwilliams): Document the purpose of this file.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _YETI_TIME_STEP_POLICY_H_
+#define _YETI_TIME_STEP_POLICY_H_
+
+#include "yeti/config.h"
+#include "yeti/linkage.h"
+#include "yeti/foundation.h"
+
+namespace yeti {
+
+/// ...
+class YETI_PUBLIC TimeStepPolicy {
+ YETI_DISALLOW_COPYING(TimeStepPolicy);
+
+ public:
+  enum Type {
+    /// Unknown.
+    UNKNOWN  = 0,
+    /// Variable.
+    VARIABLE = 1,
+    /// Fixed.
+    FIXED    = 2,
+    /// Smoothed.
+    SMOOTHED = 3
+  };
+
+ public:
+  struct Description {
+    Type type;
+    union {
+      struct {
+      } variable;
+
+      struct {
+        f32 delta_time_per_step;
+      } fixed;
+
+      struct {
+        u32 history;
+        u32 outliers;
+        f32 momentum;
+        bool payback;
+      } smoothed;
+    } config;
+  };
+
+ private:
+  struct State {
+    union {
+      struct {
+      } variable;
+
+      struct {
+        f32 accumulated;
+      } fixed;
+
+      struct {
+        f32 *history;
+        f32 debt;
+      } smoothed;
+    };
+  };
+
+ private:
+  TimeStepPolicy();
+  ~TimeStepPolicy();
+
+ public:
+  static TimeStepPolicy *create(const TimeStepPolicy::Description &desc);
+  void destroy();
+
+ public:
+  // TODO(mtwilliams): Document this interface.
+  void update(const foundation::HighResolutionTimer *frame,
+              const foundation::HighResolutionTimer *wall);
+
+ public:
+  // TODO(mtwilliams): Document this interface.
+  u32 steps() const;
+  f32 delta_time_per_step() const;
+
+ private:
+  Description desc_;
+  State state_;
+  u32 steps_;
+  f32 delta_time_per_step_;
+};
+
+YETI_INLINE u32 TimeStepPolicy::steps() const {
+  return steps_;
+}
+
+YETI_INLINE f32 TimeStepPolicy::delta_time_per_step() const {
+  return delta_time_per_step_;
+}
+
+} // yeti
+
+#endif // _YETI_TIME_STEP_POLICY_H_
---
+++ src/yeti/time_step_policy.cc
@@ -0,0 +1,67 @@
+//===-- yeti/time_step_policy.cc --------------------------*- mode: C++ -*-===//
+//
+//                             __ __     _   _
+//                            |  |  |___| |_|_|
+//                            |_   _| -_|  _| |
+//                              |_| |___|_| |_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "yeti/time_step_policy.h"
+
+namespace yeti {
+
+TimeStepPolicy::TimeStepPolicy() {
+  desc_.type = TimeStepPolicy::UNKNOWN;
+  steps_ = 0;
+  delta_time_per_step_ = 0.f;
+}
+
+TimeStepPolicy::~TimeStepPolicy() {
+}
+
+TimeStepPolicy *TimeStepPolicy::create(const TimeStepPolicy::Description &desc) {
+  // TODO(mtwilliams): Perform more thorough validations.
+  yeti_assert_development(desc.type != TimeStepPolicy::UNKNOWN);
+
+  TimeStepPolicy *tsp = new (foundation::heap()) TimeStepPolicy();
+  tsp->desc_ = desc;
+  memset((void *)&tsp->state_, 0, sizeof(TimeStepPolicy::State));
+
+  // TODO(mtwilliams): Implement the smoothed time-step-policy.
+  yeti_assert_release(desc.type != TimeStepPolicy::SMOOTHED);
+
+  return tsp;
+}
+
+void TimeStepPolicy::destroy() {
+  delete this;
+}
+
+void TimeStepPolicy::update(const foundation::HighResolutionTimer *frame,
+                            const foundation::HighResolutionTimer *wall) {
+  yeti_assert_debug(frame != NULL);
+  yeti_assert_debug(wall != NULL);
+
+  switch (desc_.type) {
+    case TimeStepPolicy::VARIABLE: {
+      steps_ = 1;
+      delta_time_per_step_ = (f32)frame->msecs() / 1000000.f;
+    } break;
+
+    case TimeStepPolicy::FIXED: {
+      state_.fixed.accumulated += (f32)frame->msecs() / 1000000.f;
+      steps_ = state_.fixed.accumulated / desc_.config.fixed.delta_time_per_step;
+      state_.fixed.accumulated -= steps_ * desc_.config.fixed.delta_time_per_step;
+      delta_time_per_step_ = desc_.config.fixed.delta_time_per_step;
+    } break;
+
+    case TimeStepPolicy::SMOOTHED: {
+      YETI_TRAP();
+    } break;
+  }
+}
+
+} // yeti
