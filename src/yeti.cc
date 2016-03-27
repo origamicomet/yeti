efa2b58315a3c192c0f34a4b148aa8ef31662ba1
Michael Williams <m.t.williams@live.com>
2016-03-27 00:06:42 -0700

    Drop superfluous symbol visibility from `*.cc`.

--- src/yeti.cc
+++ src/yeti.cc
@@ -13,28 +13,23 @@
 
 YETI_BEGIN_EXTERN_C // {
 
-YETI_PUBLIC
-void __yeti__(void)
-{
+void __yeti__(void) {
   // HACK(mtwilliams): Export at least one symbol. This is to make sure shitty
   // linkers like Microsoft's actually produce a dynamically-linked library.
 }
 
-YETI_PUBLIC
 const char *__yeti_copyright__() {
   // Copyright 2013-20xx Origami Comet Games, Inc. All rights reserved.
   // Refer to our Rakefile for details.
   return __YETI_COPYRIGHT__;
 }
 
-YETI_PUBLIC
 const char *__yeti_version__() {
   // The hash of the commit this was built from: `git rev-parse HEAD`.
   // Refer to our Rakefile for details.
   return __YETI_VERSION__;
 }
 
-YETI_PUBLIC
 yeti_uint32_t __yeti_revision__() {
   // The number of commits composing the code this was built from: `git rev-list --count HEAD`.
   // Refer to our Rakefile for details.
