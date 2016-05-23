1695bb6b0f7c0dd1bff41c3eccae7597957c8b6d
Michael Williams <m.t.williams@live.com>
2016-05-23 00:06:42 -0700

    Provide platform-independent user-space mutexes.
    We could optimize this further.

---
+++ include/yeti/foundation/mutex.h
@@ -0,0 +1,75 @@
+//===-- yeti/foundation/mutex.h ---------------------------*- mode: C++ -*-===//
+//
+//                 _____               _     _   _
+//                |   __|___ _ _ ___ _| |___| |_|_|___ ___
+//                |   __| . | | |   | . | .'|  _| | . |   |
+//                |__|  |___|___|_|_|___|__,|_| |_|___|_|_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+//
+// TODO(mtwilliams): Document the purpose of this file.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _YETI_FOUNDATION_MUTEX_H_
+#define _YETI_FOUNDATION_MUTEX_H_
+
+#include "yeti/config.h"
+#include "yeti/linkage.h"
+
+#include "yeti/foundation/types.h"
+#include "yeti/foundation/support.h"
+#include "yeti/foundation/assert.h"
+
+namespace yeti {
+namespace foundation {
+
+/// A lightweight user-space mutex.
+class YETI_PUBLIC Mutex {
+ YETI_DISALLOW_CONSTRUCTION(Mutex);
+ YETI_DISALLOW_COPYING(Mutex);
+
+ public:
+  /// \brief Creates a lightweight user-space mutex.
+  static Mutex *create(u32 spin = 0);
+
+  /// \brief Destroys this mutex.
+  void destroy();
+
+ public:
+  /// \brief Locks this mutex, waiting on other threads indefinitely.
+  void lock();
+
+  /// \brief Attmpts to lock this mutex.
+  bool try_lock();
+
+  /// \brief Unlocks this mutex.
+  void unlock();
+
+ private:
+  struct Implementation;
+};
+
+/// \def YETI_SCOPED_LOCK
+/// \brief Acquires a lock for the duration of this scope.
+#define YETI_SCOPED_LOCK(_Mutex) \
+  ScopedLock __lock_##__LINE__##__(_Mutex);
+
+/// Acquires a lock for the duration of its scope.
+class ScopedLock {
+ YETI_DISALLOW_COPYING(ScopedLock);
+
+ public:
+  ScopedLock(Mutex *mutex) : m(mutex) { m->lock(); }
+  ~ScopedLock() { m->unlock(); }
+
+ private:
+  Mutex *m;
+};
+
+} // foundation
+} // yeti
+
+#endif // _YETI_FOUNDATION_MUTEX_H_
---
+++ src/yeti/foundation/mutex.cc
@@ -0,0 +1,81 @@
+//===-- yeti/foundation/mutex.cc --------------------------*- mode: C++ -*-===//
+//
+//                 _____               _     _   _
+//                |   __|___ _ _ ___ _| |___| |_|_|___ ___
+//                |   __| . | | |   | . | .'|  _| | . |   |
+//                |__|  |___|___|_|_|___|__,|_| |_|___|_|_|
+//
+//       This file is distributed under the terms described in LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "yeti/foundation/mutex.h"
+
+#include "yeti/foundation/global_heap_allocator.h"
+
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  #include <windows.h>
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+
+namespace yeti {
+namespace foundation {
+
+struct Mutex::Implementation {
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  CRITICAL_SECTION cs;
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+};
+
+Mutex *Mutex::create(u32 spin) {
+  Implementation *impl = new (foundation::heap()) Implementation();
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  ::InitializeCriticalSectionAndSpinCount(&impl->cs, spin);
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+  return (Mutex *)impl;
+}
+
+void Mutex::destroy() {
+  Implementation *impl = (Implementation *)this;
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  ::DeleteCriticalSection(&impl->cs);
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+  delete impl;
+}
+
+void Mutex::lock() {
+  Implementation *impl = (Implementation *)this;
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  ::EnterCriticalSection(&impl->cs);
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+}
+
+bool Mutex::try_lock() {
+  Implementation *impl = (Implementation *)this;
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  return (::TryEnterCriticalSection(&impl->cs) != 0);
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+}
+
+void Mutex::unlock() {
+  Implementation *impl = (Implementation *)this;
+#if YETI_PLATFORM == YETI_PLATFORM_WINDOWS
+  ::LeaveCriticalSection(&impl->cs);
+#elif YETI_PLATFORM == YETI_PLATFORM_MAC_OS_X
+#elif YETI_PLATFORM == YETI_PLATFORM_LINUX
+#endif
+}
+
+} // foundation
+} // yeti
