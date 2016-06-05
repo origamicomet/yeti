8d1c1d680215fbe8981829735d2a5feaf15b12e7
Michael Williams <m.t.williams@live.com>
2016-06-05 16:13:34 -0700

    Add `fnv1a` hash. Provide 32 and 64 bit variants for each hash.

--- include/yeti/foundation/hash.h
+++ include/yeti/foundation/hash.h
@@ -26,11 +26,29 @@
 namespace yeti {
 namespace foundation {
 
-/// Calculates the murmur hash of @buf with the @seed.
-extern YETI_PUBLIC u64 murmur(const char *buf, u64 seed = 0ull);
+/// Calculates the 32-bit FNV1A hash of @buf.
+extern YETI_PUBLIC u32 fnv1a_hash_32(const char *buf);
 
-/// Calculates the murmur hash of @buf with the @seed.
-extern YETI_PUBLIC u64 murmur(const uintptr_t buf, u64 buf_len, u64 seed = 0ull);
+/// Calculates the 32-bit FNV1A hash of @buf.
+extern YETI_PUBLIC u32 fnv1a_hash_32(const void *buf, u32 buf_len);
+
+/// Calculates the 64-bit FNV1A hash of @buf.
+extern YETI_PUBLIC u64 fnv1a_hash_64(const char *buf);
+
+/// Calculates the 64-bit FNV1A hash of @buf.
+extern YETI_PUBLIC u64 fnv1a_hash_64(const void *buf, u64 buf_len);
+
+/// Calculates the 32-bit murmur hash of @buf with the @seed.
+extern YETI_PUBLIC u32 murmur_hash_32(const char *buf, u32 seed = 0ull);
+
+/// Calculates the 32-bit murmur hash of @buf with the @seed.
+extern YETI_PUBLIC u32 murmur_hash_32(const void *buf, u32 buf_len, u32 seed = 0ull);
+
+/// Calculates the 64-bit murmur hash of @buf with the @seed.
+extern YETI_PUBLIC u64 murmur_hash_64(const char *buf, u64 seed = 0ull);
+
+/// Calculates the 64-bit murmur hash of @buf with the @seed.
+extern YETI_PUBLIC u64 murmur_hash_64(const void *buf, u64 buf_len, u64 seed = 0ull);
 
 } // foundation
 } // yeti
--- src/yeti/foundation/hash.cc
+++ src/yeti/foundation/hash.cc
@@ -22,16 +22,88 @@
 namespace yeti {
 namespace foundation {
 
-u64 murmur(const char *buf, u64 seed) {
+u32 fnv1a_hash_32(const char *buf) {
   yeti_assert_debug(buf != NULL);
-  return murmur((const uintptr_t)buf, strlen(buf), seed);
+  return fnv1a_hash_32((const void *)buf, strlen(buf));
 }
 
-u64 murmur(const uintptr_t buf, u64 buf_len, u64 seed) {
+u32 fnv1a_hash_32(const void *buf, u32 buf_len) {
+  const u8 *start = (const u8 *)buf;
+  const u8 *end = start + buf_len;
+  u32 hash = 2166136261ul;
+  for (const u8 *I = start; I != end; ++I)
+    hash = (hash ^ *I) * 16777619ul;
+  return hash;
+}
+
+u64 fnv1a_hash_64(const char *buf) {
+  yeti_assert_debug(buf != NULL);
+  return fnv1a_hash_64((const void *)buf, strlen(buf));
+}
+
+u64 fnv1a_hash_64(const void *buf, u64 buf_len) {
+  const u8 *start = (const u8 *)buf;
+  const u8 *end = start + buf_len;
+  u64 hash = 14695981039346656037ull;
+  for (const u8 *I = start; I != end; ++I)
+    hash = (hash ^ *I) * 1099511628211ull;
+  return hash;
+}
+
+u32 murmur_hash_32(const char *buf, u32 seed) {
+  yeti_assert_debug(buf != NULL);
+  return murmur_hash_32((const void *)buf, strlen(buf), seed);
+}
+
+u32 murmur_hash_32(const void *buf, u32 buf_len, u32 seed) {
+  yeti_assert_debug(buf != NULL);
+
+  static const u32 m = 0x5bd1e995ul;
+  static const u32 r = 24ul;
+
+  u32 h = seed ^ buf_len;
+
+  const u8 *data = (const u8 *)buf;
+
+  while (buf_len >= 4) {
+    u32 k = *(const u32 *)data;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h *= m;
+    h ^= k;
+
+    data += 4;
+    buf_len -= 4;
+  }
+
+  const u8 *stragglers = (const u8 *)data;
+  switch (buf_len) {
+    case 3: h ^= stragglers[2] << 16ul;
+    case 2: h ^= stragglers[1] << 8ul;
+    case 1: h ^= stragglers[0];
+            h *= m;
+  }
+
+  h ^= h >> 13ul;
+  h *= m;
+  h ^= h >> 15ul;
+
+  return h;
+}
+
+u64 murmur_hash_64(const char *buf, u64 seed) {
+  yeti_assert_debug(buf != NULL);
+  return murmur_hash_64((const void *)buf, strlen(buf), seed);
+}
+
+u64 murmur_hash_64(const void *buf, u64 buf_len, u64 seed) {
   yeti_assert_debug(buf != NULL);
 #if YETI_ARCHITECTURE == YETI_ARCHITECTURE_X86
-  static const u32 m = 0x5bd1e995;
-  static const u32 r = 24;
+  static const u32 m = 0x5bd1e995ul;
+  static const u32 r = 24ul;
 
   u32 h1 = u32(seed) ^ buf_len;
   u32 h2 = u32(seed >> 32);
@@ -57,10 +129,11 @@ u64 murmur(const uintptr_t buf, u64 buf_len, u64 seed) {
     buf_len -= 4;
   }
 
+  const u8 *stragglers = (const u8 *)data;
   switch (buf_len) {
-    case 3: h2 ^= ((unsigned char *)data)[2] << 16ul;
-    case 2: h2 ^= ((unsigned char *)data)[1] << 8ul;
-    case 1: h2 ^= ((unsigned char *)data)[0];
+    case 3: h2 ^= ((const u8 *)stragglers)[2] << 16ul;
+    case 2: h2 ^= ((const u8 *)stragglers)[1] << 8ul;
+    case 1: h2 ^= ((const u8 *)stragglers)[0];
             h2 *= m;
   }
 
@@ -72,7 +145,7 @@ u64 murmur(const uintptr_t buf, u64 buf_len, u64 seed) {
   return ((((u64)h1) << 32ull) | (u64)h2);
 #elif YETI_ARCHITECTURE == YETI_ARCHITECTURE_X86_64
   static const u64 m = 0xc6a4a7935bd1e995ull;
-  static const u64 r = 47;
+  static const u64 r = 47ull;
 
   u64 h = seed ^ (buf_len * m);
 
@@ -88,8 +161,7 @@ u64 murmur(const uintptr_t buf, u64 buf_len, u64 seed) {
     h *= m;
   }
 
-  const unsigned char *stragglers = (const unsigned char *)data;
-
+  const u8 *stragglers = (const u8 *)data;
   switch (buf_len & 7) {
     case 7: h ^= u64(stragglers[6]) << 48ull;
     case 6: h ^= u64(stragglers[5]) << 40ull;
