972ae5504869c42375fa4dc48d6014fef88d18fa
Michael Williams <m.t.williams@live.com>
2017-03-25 14:45:01 -0700

    Improve `thread_safe::Pool<T>`.
    * Allow 'em to allocate their own memory.
    * Zero-memory prior to returning pooled elements.

--- include/yeti/foundation/thread_safe/pool.h
+++ include/yeti/foundation/thread_safe/pool.h
@@ -42,34 +42,62 @@ class Pool {
   };
 
  public:
+  Pool(Allocator &allocator, size_t n);
   Pool(uintptr_t mem, size_t sz);
   ~Pool();
+
+ private:
+  void initialize();
 
  public:
   T *acquire();
   void relinquish(T *borrowed);
 
  private:
-  Element *head_;
+  Allocator *allocator_;
+  uintptr_t lower_, upper_;
+  Element *free_;
 };
 
 template <typename T>
+Pool<T>::Pool(Allocator &allocator, size_t n) {
+  allocator_ = &allocator;
+
+  lower_ = allocator.allocate(n * sizeof(Element));
+  upper_ = lower_ + (n * sizeof(Element));
+
+  this->initialize();
+}
+
+template <typename T>
 Pool<T>::Pool(uintptr_t mem, size_t sz) {
-  static const size_t N = sz / sizeof(Element);
-  head_ = (Element *)mem;
-  for (Element *E = head_; E < &head_[N-1]; ++E)
-    E->next_available_element = &E[1];
-  head_[N-1].next_available_element = NULL;
+  allocator_ = NULL;
+
+  lower_ = mem;
+  upper_ = mem + sz;
+
+  this->initialize();
 }
 
 template <typename T>
 Pool<T>::~Pool() {
+  if (allocator_)
+    allocator_->deallocate(lower_);
+}
+
+template <typename T>
+void Pool<T>::initialize() {
+  const size_t N = (upper_ - lower_) / sizeof(Element);
+  free_ = (Element *)lower_;
+  for (Element *E = free_; E < &free_[N-1]; ++E)
+    E->next_available_element = &E[1];
+  free_[N-1].next_available_element = NULL;
 }
 
 template <typename T>
 T *Pool<T>::acquire() {
 try_acquire:
-  Element *element = (Element *)atomic::load((void **volatile const)&head_);
+  Element *element = (Element *)atomic::load((void **volatile const)&free_);
 
   if (element == NULL)
     // No more space.
@@ -77,21 +105,27 @@ try_acquire:
 
   Element *next = (Element *)atomic::load((void **volatile const)&element->next_available_element);
 
-  if (atomic::cmp_and_xchg((void ** volatile)&head_, (void *)element, (void *)next) != (void *)element)
+  if (atomic::cmp_and_xchg((void ** volatile)&free_, (void *)element, (void *)next) != (void *)element)
     goto try_acquire;
+
+  // TODO(mtwilliams): Use type traits to call `T`'s default constructor.
+  memset((void *)&element->storage, 0, sizeof(T));
 
   return &element->storage;
 }
 
 template <typename T>
 void Pool<T>::relinquish(T *borrowed) {
+  yeti_assert_debug((uintptr_t)borrowed >= lower_);
+  yeti_assert_debug((uintptr_t)borrowed < upper_);
+
   Element *element = (Element *)borrowed;
 
 try_relinquish:
-  Element *head = (Element *)atomic::load((void **volatile const)&head_);
+  Element *head = (Element *)atomic::load((void **volatile const)&free_);
   element->next_available_element = head;
 
-  if (atomic::cmp_and_xchg((void ** volatile)&head_, (void *)head, (void *)element) != (void *)head)
+  if (atomic::cmp_and_xchg((void ** volatile)&free_, (void *)head, (void *)element) != (void *)head)
     goto try_relinquish;
 }
 
