6f6fc1f1cd486c31915cdefddd7148fda1f52c25
Michael Williams <m.t.williams@live.com>
2017-09-05 15:56:53 -0700

    Improve code style.

--- include/yeti/foundation/filesystem.h
+++ include/yeti/foundation/filesystem.h
@@ -61,7 +61,7 @@ struct Info {
   u64 size;
 
   /// Created at, in seconds from Unix epoch.
-  /// \warning Is not available on some platforms, namely macOS and Linux!
+  /// \warning Is not available on some platforms, namely Mac and Linux!
   u64 created_at;
 
   /// Most recently accessed at, in seconds from Unix epoch.
@@ -78,12 +78,12 @@ enum Permissions {
   /// Write access.
   WRITE = 0x00000002,
 
-  /// Don't allow others to fiddle.
+  /// Don't allow others to read, write, or destroy.
   EXCLUSIVE = 0x80000000
 };
 
 enum Position {
-  /// Move from the start or end of a file.
+  /// Move from the beginning or end of the file.
   ABSOLUTE = 1,
 
   /// Move from current position in the file.
@@ -114,15 +114,21 @@ extern YETI_PUBLIC bool info(fs::File *file, fs::Info *info);
 extern YETI_PUBLIC bool exists(const char *path);
 
 /// Creates a new file or directory at @path.
+///
 /// \note Fails if a file or directory already exists at @path.
+///
 extern YETI_PUBLIC bool create(fs::Type type, const char *path);
 
 /// Destroys a file or directory at @path.
+///
 /// \note Auto-detects the type from @path.
+///
 extern YETI_PUBLIC bool destroy(const char *path);
 
 /// Destroys a file or directory at @path.
+///
 /// \note Fails if @type does not match the entry at @path.
+///
 extern YETI_PUBLIC bool destroy(fs::Type type, const char *path);
 
 /// User-specified callback for yeti::foundation::fs::walk.
@@ -130,17 +136,21 @@ typedef bool (*Walker)(const char *path, const Info *info, void *ctx);
 
 /// Non-recursively walks @directory with @walker until there are no more
 /// entires or @walker returns `false`.
-extern YETI_PUBLIC bool walk(const char *directory, fs::Walker walker, void *walker_ctx = NULL);
+extern YETI_PUBLIC bool walk(const char *directory,
+                             fs::Walker walker,
+                             void *walker_ctx = NULL);
 
 /// An opaque handle to a directory being watched.
 struct Watch;
 
-/// User-specified callback for yeti::foundation::fs::watch.
+/// User-specified callback for `yeti::foundation::fs::watch`.
 typedef void (*Watcher)(Event event, const char *path, void *ctx);
 
 /// Watches @directory's tree for changes, indicating to @callback what changes
 /// occur.
-extern YETI_PUBLIC Watch *watch(const char *directory, fs::Watcher callback, void *callback_ctx = NULL);
+extern YETI_PUBLIC Watch *watch(const char *directory,
+                                fs::Watcher callback,
+                                void *callback_ctx = NULL);
 
 // TODO(mtwilliams): Document this.
 extern YETI_PUBLIC void poll(Watch *watch);
@@ -152,25 +162,33 @@ extern YETI_PUBLIC void unwatch(Watch *watch);
 extern YETI_PUBLIC fs::File *create_or_open(const char *path, const u32 permissions);
 
 /// Opens an existing file @path with @permissions.
+///
 /// \note Fails if there is no file @path.
+///
 extern YETI_PUBLIC fs::File *open(const char *path, const u32 permissions);
 
 /// Closes a @file opened by yeti::foundation::fs::open.
 extern YETI_PUBLIC void close(fs::File *file);
 
 /// Synchronously reads up to @in_len bytes from the @file into @in.
+///
 /// \returns The number of bytes actually read. Zero indicates failure.
+///
 extern YETI_PUBLIC u64 read(fs::File *file, uintptr_t in, u64 in_len);
 
 /// Synchronously writes up to @out_len bytes to the @file from @out.
+///
 /// \returns The number of bytes actually written. Zero indicates failure.
+///
 extern YETI_PUBLIC u64 write(fs::File *file, const uintptr_t out, u64 out_len);
 
-/// Moves the read/write position inside the @file. If @pos is fs::ABSOLUTE,
+/// Moves the read/write position inside the @file. If @pos is `fs::ABSOLUTE`,
 /// then @offset is taken as from the start of the file if positive, or from
-/// the end of the file if negative. If @pos is fs::RELATIVE, the @offset is
+/// the end of the file if negative. If @pos is `fs::RELATIVE`, the @offset is
 /// taken from the current position in the file.
+///
 /// \returns The number of bytes seeked.
+///
 extern YETI_PUBLIC i64 seek(fs::File *file, fs::Position pos, i64 offset);
 
 /// Forces all buffered data to be written to the @file.
