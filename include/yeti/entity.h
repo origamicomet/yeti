b9b74919027abce1058a22eb4d22d036874f3f40
Michael Williams <m.t.williams@live.com>
2017-10-02 20:06:17 -0700

    Buff out entities.
    * Can look up entities by name now.
    * Can dynamically register and unregister for lifecycle event callbacks.
    * Add `EntityReflector` which records lifecycle events to buffers.
     * Will be used to reflect lifetimes to render and audio threads in the
     future.
    * Thoroughly documented.

--- include/yeti/entity.h
+++ include/yeti/entity.h
@@ -13,6 +13,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+// TODO(mtwilliams): Cookies.
+
 #ifndef _YETI_ENTITY_H_
 #define _YETI_ENTITY_H_
 
@@ -38,9 +40,24 @@ static const u32 ENTITY_GENERATION_MASK = (1 << ENTITY_GENERATION_BITS) - 1;
 /// Offset, in bits, for generation portion of `yeti::Entity::Id`.
 static const u32 ENTITY_GENERATION_OFFSET = 24;
 
+/// \brief An entity managed by an `EntityManager`.
+///
+/// \note Entities don't actually "exist." Rather, they are weak references
+/// used to refer to entities.
+///
+/// \see yeti::EntityManager
+///
 struct Entity {
   /// Unique identifier assigned to an entity.
   typedef u32 Id;
+
+  /// Types of lifecycle events.
+  enum LifecycleEvent {
+    /// Entity was created.
+    CREATED = 1,
+    /// Entity was destroyed.
+    DESTROYED = 2
+  };
 
   /// \copydoc yeti::Entity::Id
   Id id;
@@ -53,17 +70,69 @@ struct Entity {
     : id((index << ENTITY_INDEX_OFFSET) | (generation << ENTITY_GENERATION_OFFSET))
   {}
 
-  /// Returns index of entity corresponding to this handle.
+  /// \internal Returns index of entity corresponding to this handle.
   u32 index() const {
     return (id >> ENTITY_INDEX_OFFSET) & ENTITY_INDEX_MASK;
   }
 
-  /// Returns generation corresponding to this handle.
+  /// \internal Returns generation corresponding to this handle.
   u32 generation() const {
     return (id >> ENTITY_GENERATION_OFFSET) & ENTITY_GENERATION_MASK;
   }
 };
 
+/// Manages entities.
+///
+/// # Overview
+///
+/// Entity managers create, destroy, and track entities. They also facilitate
+/// high-level management of entities through a few simple but powerful
+/// mechanisms.
+///
+/// \remark Since entities don't "exist" per se and rather are an abstraction
+/// around weak references referring to entities, you can conceptualize this as
+/// an index.
+///
+/// ## Naming
+///
+/// Entities can be _uniquely_ named and looked up by name in constant time.
+/// However, you cannot look up the name of a particular entity. This is a
+/// consequence of names being hashed to reduce memory footprint and improve
+/// performance.
+///
+/// ## Cookies & Ownership
+///
+/// One or more entities may be associated with an arbitary non-zero 32-bit
+/// integer referred to as a "cookie." At any point, various operations can be
+/// performed on any entities associated with a particular cookie. This can be
+/// used establish weak ownership semantics of entities.
+///
+/// \remark Cookies shouldn't be used to tag entities as a certain type to
+/// drive game logic. Rather, the `TagSystem` should be used. For example,
+/// each spawn point could be tagged as `spawner` and iterated on load to build
+/// internal data structures necessary for spawning (or respawning) players.
+///
+/// ## Liveliness
+///
+/// As entities are essentially weak references, the liveliness of entities can
+/// be quickly checked through `alive` and `dead`.
+///
+/// ### Garbage Collection
+///
+/// Rather than relying on callbacks, you can periodically query the liveliness
+/// of entities to perform "garbage collection" of data associated with a
+/// particular entity.
+///
+/// \warning As liveliness is implemented by counting underlying reuse of slots
+/// there is a very small possibility of erroneously considering an entity
+/// alive when in fact is dead. Good practice is to check relatively frequently
+/// to prevent that from happening.
+///
+/// ## Callbacks
+///
+/// You can dynamically register and unregister callbacks that are called
+/// whenever an entity is created or destroyed.
+///
 class YETI_PUBLIC EntityManager {
  YETI_DISALLOW_COPYING(EntityManager)
 
@@ -78,27 +147,129 @@ class YETI_PUBLIC EntityManager {
   /// Creates @n entities, storing the handles in @entities.
   void create(Entity *entities, unsigned n);
 
+  /// Destroys @entity.
+  void destroy(Entity entity);
+
   /// Determines if @entity is alive.
   bool alive(Entity entity) const;
 
   /// Determines if @entity is dead.
   bool dead(Entity entity) const;
 
-  /// Destroys @entity.
-  void destroy(Entity entity);
+  /// Names @entity.
+  void name(Entity entity, const char *name);
+
+  /// Names @entity.
+  void name(Entity entity, const u32 hash_of_name);
+
+  /// Names @n entities.
+  void name(const Entity *entities, const u32 *hashes_of_names, unsigned n);
+
+  /// Tries to find an entity named @name.
+  ///
+  /// \return If the entity with @name was found.
+  ///
+  bool named(const char *name, Entity *entity);
+
+  /// Tries to find a named entity.
+  ///
+  /// \return If the entity was found.
+  ///
+  bool named(const u32 hash_of_name, Entity *entity);
+
+ public:
+  typedef void (*LifecycleCallback)(Entity::LifecycleEvent event,
+                                    Entity entity,
+                                    void *context);
+
+  /// Registers @callback to be called whenever an entity is created or
+  /// destroyed.
+  ///
+  /// \return Identifier that can be used to unregister the callback.
+  ///
+  u32 register_lifecycle_callback(LifecycleCallback callback,
+                                  void *context = NULL);
+
+  /// Unregisters a @calblack.
+  void unregister_lifecycle_callback(u32 callback);
 
  private:
-  /// Upper limit on number of entities that can be alive.
+  /// \internal Calls all registered lifecycle callbacks.
+  /// @{
+  void notify(Entity::LifecycleEvent event, Entity entity);
+  void notify(Entity::LifecycleEvent event, const Entity *entities, size_t n);
+  /// @}
+
+ public:
+  /// Limit on number of entities that can be alive at any point in time.
+  unsigned limit() const {
+    return limit_;
+  }
+
+ private:
+  /// Limit on number of entities that can be alive at any point in time.
   const unsigned limit_;
 
-  /// Number of alive entities.
+  /// Number of entities currently alive.
   unsigned n_;
 
-  /// Generations of all entities.
-  core::Array<u8> generations_;
+  /// All entities.
+  core::Array<Entity> entities_;
+
+  /// Names associated with entities.
+  core::Array<u32> names_;
+
+  /// Map of names to entities used to accelerate lookups of entities by name.
+  core::Map<u32, Entity, core::map::IdentityHashFunction<u32>::hash> name_to_entity_;
+
+  /// Cookies associated with entities.
+  core::Array<u32> cookies_;
 
   /// Queue of available indicies.
   core::Queue<u32> free_;
+
+  struct RegisteredLifecycleCallback {
+    u32 id;
+
+    LifecycleCallback callback;
+    void *context;
+
+    explicit RegisteredLifecycleCallback(u32 id,
+                                         LifecycleCallback callback,
+                                         void *context)
+      : id(id)
+      , callback(callback)
+      , context(context)
+    {}
+  };
+
+  /// Lifecycle callbacks.
+  core::Array<RegisteredLifecycleCallback> callbacks_;
+};
+
+/// Records entity lifecycle events.
+class YETI_PUBLIC EntityReflector {
+ YETI_DISALLOW_COPYING(EntityReflector)
+
+ public:
+  EntityReflector(EntityManager *manager);
+  ~EntityReflector();
+
+ private:
+  /// \internal Records a lifecycle event to appropriate buffer.
+  void record(Entity::LifecycleEvent event, Entity entity);
+
+ private:
+  /// \internal Wraps `record` to provide a `Entity::LifecycleCallback` function signature.
+  static void shim(Entity::LifecycleEvent event, Entity entity, void *);
+
+ private:
+  EntityManager *manager_;
+
+  u32 callback_;
+
+  core::Array<Entity> created_;
+  core::Array<Entity> destroyed_;
 };
 
 } // yeti
--- src/yeti/entity.cc
+++ src/yeti/entity.cc
@@ -16,12 +16,18 @@ namespace yeti {
 EntityManager::EntityManager(unsigned size)
   : limit_(size)
   , n_(0)
-  , generations_(core::global_page_allocator(), size)
+  , entities_(core::global_page_allocator(), size)
+  , names_(core::global_page_allocator(), size)
+  , name_to_entity_(core::global_page_allocator(), size)
+  , cookies_(core::global_page_allocator(), size)
   , free_(core::global_page_allocator(), size)
+  , callbacks_(core::global_page_allocator())
 {
-  // PERF(mtwilliams): Better bulk insertions.
+  // PERF(mtwilliams): Optimize bulk insertions.
   for (unsigned index = 0; index < size; ++index) {
-    generations_[index] = 0;
+    entities_[index] = Entity(index, 0);
+    names_[index]    = 0;
+    cookies_[index]  = 0;
     free_.push(index);
   }
 }
@@ -32,14 +38,16 @@ EntityManager::~EntityManager() {
 Entity EntityManager::create() {
   yeti_assert_debug(n_ <= limit_);
 
-  u32 index, generation;
+  u32 slot;
 
-  free_.pop(&index);
-  generation = generations_[index];
-
-  const Entity entity = Entity(index, generation);
+  // Grab next available slot.
+  free_.pop(&slot);
 
   n_ += 1;
+
+  const Entity entity = entities_[slot];
+
+  this->notify(Entity::CREATED, entity);
 
   return entity;
 }
@@ -49,36 +57,160 @@ void EntityManager::create(Entity *entities, unsigned n) {
   yeti_assert_debug(n_ + n <= limit_);
 
   for (unsigned entity = 0; entity < n; ++entity) {
-    u32 index, generation;
+    u32 slot;
 
-    free_.pop(&index);
-    generation = generations_[index];
+    // Grab next available slot.
+    free_.pop(&slot);
 
-    entities[entity] = Entity(index, generation);
+    entities[entity] = entities_[slot];
   }
 
   n_ += n;
-}
 
-bool EntityManager::alive(Entity entity) const {
-  return (generations_[entity.index()] == entity.generation());
-}
-
-bool EntityManager::dead(Entity entity) const {
-  return (generations_[entity.index()] != entity.generation());
+  // Deferred to reduce instruction cache evictions and improve branch prediction.
+  this->notify(Entity::CREATED, entities, n);
 }
 
 void EntityManager::destroy(Entity entity) {
-  // Prevent users from destroying entities
+  if (alive(entity)) {
+    const u32 index = entity.index();
+    const u8 generation = entity.generation();
+
+    // Generation is bumped when destroyed, rather than when allocated, to
+    // catch dead entities right away.
+    entities_[index] = Entity(index, generation + 1);
+
+    if (const u32 name = names_[index])
+      name_to_entity_.remove(name);
+
+    names_[index] = 0;
+    cookies_[index] = 0;
+
+    // Slot will be reused last.
+    free_.push(index);
+
+    n_ -= 1;
+
+    this->notify(Entity::DESTROYED, entity);
+  }
+}
+
+bool EntityManager::alive(Entity entity) const {
+  // PERF(mtwilliams): Check without shifting.
+  return (entities_[entity.index()].generation() == entity.generation());
+}
+
+bool EntityManager::dead(Entity entity) const {
+  // PERF(mtwilliams): Check without shifting.
+  return (entities_[entity.index()].generation() != entity.generation());
+}
+
+void EntityManager::name(Entity entity, const char *name) {
+  yeti_assert_debug(name != NULL);
+
+  const u32 hash_of_name = core::murmur_hash_32(name);
+
+  this->name(entity, hash_of_name);
+}
+
+void EntityManager::name(Entity entity, const u32 hash_of_name) {
   yeti_assert_debug(alive(entity));
+  yeti_assert_debug(hash_of_name != 0);
 
-  // Generation is bumped when destroyed, rather than when allocated, to catch
-  // dead entities right away.
-  generations_[entity.index()] += 1;
+  name_to_entity_.insert(hash_of_name, entity);
+  names_[entity.index()] = hash_of_name;
+}
 
-  free_.push(entity.index());
+void EntityManager::name(const Entity *entities, const u32 *hashes_of_names, unsigned n) {
+  yeti_assert_debug(entities != NULL);
+  yeti_assert_debug(hashes_of_names != NULL);
 
-  n_ -= 1;
+  for (unsigned entity = 0; entity < n; ++entity) {
+    name_to_entity_.insert(hashes_of_names[entity], entities[entity]);
+    names_[entities[entity].index()] = hashes_of_names[entity];
+  }
+}
+
+bool EntityManager::named(const char *name, Entity *entity) {
+  yeti_assert_debug(name != NULL);
+
+  const u32 hash_of_name = core::murmur_hash_32(name);
+
+  return this->named(hash_of_name, entity);
+}
+
+bool EntityManager::named(const u32 hash_of_name, Entity *entity) {
+  yeti_assert_debug(hash_of_name != 0);
+  yeti_assert_debug(entity != NULL);
+
+  if (const Entity *ptr_to_entity = name_to_entity_.find(hash_of_name)) {
+    *entity = *ptr_to_entity;
+    return true;
+  }
+
+  return false;
+}
+
+u32 EntityManager::register_lifecycle_callback(LifecycleCallback callback,
+                                               void *context) {
+  static u32 id = 0;
+
+  callbacks_.push(RegisteredLifecycleCallback(++id, callback, context));
+
+  return id;
+}
+
+void EntityManager::unregister_lifecycle_callback(u32 callback) {
+  for (u32 index = 0; index < callbacks_.size(); ++index) {
+    if (callbacks_[index].id != callback)
+      continue;
+
+    // Swap 'n' pop.
+    callbacks_[index - 1] = callbacks_[callbacks_.size() - 1];
+    callbacks_.pop();
+
+    break;
+  }
+}
+
+void EntityManager::notify(Entity::LifecycleEvent event, Entity entity) {
+  for (const RegisteredLifecycleCallback *I = callbacks_.begin(); I != callbacks_.end(); ++I)
+    I->callback(event, entity, I->context);
+}
+
+void EntityManager::notify(Entity::LifecycleEvent event, const Entity *entities, size_t n) {
+  for (const RegisteredLifecycleCallback *I = callbacks_.begin(); I != callbacks_.end(); ++I)
+    for (unsigned entity = 0; entity < n; ++entity)
+      I->callback(event, entities[entity], I->context);
+}
+
+EntityReflector::EntityReflector(EntityManager *manager)
+  : manager_(manager)
+  , callback_(manager_->register_lifecycle_callback(&shim, (void *)this))
+  , created_(core::global_heap_allocator())
+  , destroyed_(core::global_heap_allocator())
+{
+}
+
+EntityReflector::~EntityReflector() {
+}
+
+void EntityReflector::record(Entity::LifecycleEvent event, Entity entity) {
+  switch (event) {
+    case Entity::CREATED:
+      created_.push(entity);
+      break;
+
+    case Entity::DESTROYED:
+      destroyed_.push(entity);
+      break;
+  }
+}
+
+void EntityReflector::shim(Entity::LifecycleEvent event,
+                           Entity entity,
+                           void *entity_reflector_ptr) {
+  ((EntityReflector *)entity_reflector_ptr)->record(event, entity);
 }
 
 } // yeti
