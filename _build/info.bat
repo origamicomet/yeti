3bb919dd15b202629d7ce88c39bb3217cc7f61d4
Michael Williams <m.t.williams@live.com>
2018-02-04 22:36:26 -0800

    Fix build timestamping.

--- _build/info.bat
+++ _build/info.bat
@@ -5,14 +5,14 @@
 for /f "tokens=2 delims==" %%r in ('wmic OS Get localdatetime /value') do (
   set T=%%r
 
-  set YYYY=%T:~0,4%
-  set MM=%T:~4,2%
-  set DD=%T:~6,2%
-  set HH=%T:~8,2%
-  set MM=%T:~10,2%
-  set SS=%T:~12,2%
+  set YYYY=!T:~0,4!
+  set MM=!T:~4,2!
+  set DD=!T:~6,2!
+  set HH=!T:~8,2!
+  set MM=!T:~10,2!
+  set SS=!T:~12,2!
 
-  set TIMESTAMP=%YYYY%-%MM%-%DD% %HH%:%MM%:%SS%
+  set TIMESTAMP=!YYYY!-!MM!-!DD! !HH!:!MM!:!SS!
 )
 
 set YETI_COPYRIGHT=Copyright 2012-%YYYY% Origami Comet Games, Inc.
