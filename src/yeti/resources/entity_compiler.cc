da4d6608c6f5c70dd335334b3f4fd5fc46710def
Michael Williams <m.t.williams@live.com>
2018-02-05 20:06:21 -0800

    Simplify entity resource validation.

--- src/yeti/resources/entity_compiler.cc
+++ src/yeti/resources/entity_compiler.cc
@@ -67,74 +67,39 @@ bool EntityCompiler::load() {
   u8 scratch[SIZE_OF_SCRATCH];
 
   // Parse into tree.
-  if (xml_parse_into_memory((const char *)document_.raw(),
-                            (void *)scratch,
-                            SIZE_OF_SCRATCH,
-                            (void *)heap_.raw(),
-                            SIZE_OF_HEAP,
-                            &root_) != XML_OK)
-    return false;
-
-  return true;
+  return (xml_parse_into_memory((const char *)document_.raw(),
+                                (void *)scratch,
+                                SIZE_OF_SCRATCH,
+                                (void *)heap_.raw(),
+                                SIZE_OF_HEAP,
+                                &root_) == XML_OK);
 }
 
 // TODO(mtwilliams): Contextualize errors and warnings.
 
-#define BAD_MARKUP_DECLARATION \
-  return env_->warning(env_, "Bad markup declaration."), true;
-#define BAD_DOCUMENT_ENCODING \
-  return env_->error(env_, "Bad document encoding."), false;
-#define NOT_AN_ENTITY \
-  return env_->error(env_, "Expected an entity definition but got \"%.*s\" instead.", e->name.l, e->name.s), false;
-
 bool EntityCompiler::validate() {
   xml_element_t *e = root_->children;
 
-  // Validate optional declaration if present.
-  if (core::string::compare("?xml", e->name.s, e->name.l)) {
-    if (e->num_of_attributes < 1) {
-      // Version is mandatory.
-      BAD_MARKUP_DECLARATION;
-    }
+  // Let subsequent passes know there's a markup declaration.
+  document_has_declaration_ = core::string::compare("?xml", e->name.s, e->name.l);
 
-    const xml_fragment_t *name, *value;
-
-    name = &e->attributes[0].name;
-    value = &e->attributes[0].value;
-
-    if (!core::string::compare("version", name->s, name->l))
-      BAD_MARKUP_DECLARATION;
-
-    if (!core::string::compare("1.0", value->s, value->l))
-      BAD_MARKUP_DECLARATION;
-
-    if (e->num_of_attributes > 2) {
-      name = &e->attributes[1].name;
-      value = &e->attributes[1].value;
-
-      // We expect encoding to follow.
-      if (!core::string::compare("encoding", name->s, name->l))
-        BAD_MARKUP_DECLARATION;
-
-      // We only support a sane encoding.
-      if (!core::string::compare("UTF-8", value->s, value->l))
-        BAD_DOCUMENT_ENCODING;
-    }
-
-    // Let subsequent passes know that they need to skip the declaration.
-    document_has_declaration_ = true;
-
+  // Skip markup declaration if present.
+  if (document_has_declaration_)
     e = e->sibling;
-  }
 
   bool valid = true;
 
-  // Everything remaining should be entity definitions.
+  // Everything else should be entity definitions.
   while (e) {
-    if (!core::string::compare("entity", e->name.s, e->name.l))
-      NOT_AN_ENTITY;
+    const bool is_entity_definition = core::string::compare("entity", e->name.s, e->name.l);
 
-    if (!this->validate_an_entity_definition(e))
+    if (!is_entity_definition)
+      env_->error(env_, "Expected an entity definition but got \"%.*s\" instead.", e->name.l, e->name.s);
+
+    if (is_entity_definition)
+      if (!this->validate_an_entity_definition(e))
+        valid = false;
+    else
       valid = false;
 
     e = e->sibling;
@@ -143,23 +108,19 @@ bool EntityCompiler::validate() {
   return valid;
 }
 
-
-#undef BAD_MARKUP_DECLARATION
-#undef BAD_DOCUMENT_ENCODING
-#undef NOT_AN_ENTITY
-
 bool EntityCompiler::validate_an_entity_definition(xml_element_t *e) {
   bool valid = true;
 
-  // Check for an identifier.
+  // Check for identifier.
   if (e->num_of_attributes >= 1) {
     const xml_fragment_t *name = &e->attributes[0].name,
                          *value = &e->attributes[0].value;
 
     if (core::string::compare("id", name->s, name->l)) {
-      // Check that the identifier is a well-formed UUID.
-      if ((value->l != 36) || !core::uuid::validate(value->s))
+      if (!core::uuid::validate(value->s)) {
         env_->error(env_, "Expected an identifier but got \"%.*s\" instead.", value->l, value->s);
+        valid = false;
+      }
     } else {
       env_->error(env_, "Expected an identifier but got \"%.*s\" instead.", name->l, name->s);
       valid = false;
@@ -168,34 +129,24 @@ bool EntityCompiler::validate_an_entity_definition(xml_element_t *e) {
     env_->error(env_, "Entity not assigned an identifier. Will automatically be assigned one.");
   }
 
+  // Ignore superfluous attributes.
   if (e->num_of_attributes >= 2) {
     env_->warning(env_, "Ignoring superfluous attributes on entity definition.");
   }
 
-  static const struct {
-    const char *name;
-    bool (EntityCompiler::*validate)(xml_element_t *);
-  } validations[] = {
-    { "components", &EntityCompiler::validate_all_component_definitions },
-    { "children",   &EntityCompiler::validate_all_children_definitions }
-  };
-
+  // Validate components and children if any.
   for (e = e->children; e; e = e->sibling) {
-    for (unsigned v = 0; v < YETI_ELEMENTS_IN_ARRAY(validations); ++v) {
-      if (core::string::compare(validations[v].name, e->name.s, e->name.l)) {
-        if (!(this->*validations[v].validate)(e))
-          valid = false;
-        goto validated;
-      }
+    if (core::string::compare("components", e->name.s, e->name.l)) {
+      if (!this->validate_all_component_definitions(e))
+        valid = false;
+    } else if (core::string::compare("children", e->name.s, e->name.l)) {
+      if (!this->validate_all_children_definitions(e))
+        valid = false;
+    } else {
+      // Egregious enough that it can be ignored.
+      env_->error(env_, "Expected component or children definition but got \"%.*s\" instead.", e->name.l, e->name.s);
+      valid = false;
     }
-
-    env_->error(env_, "Expected component or child definitions but got \"%.*s\" instead.", e->name.l, e->name.s);
-
-    // Egregious enough that it should be fixed.
-    valid = false;
-
-  validated:
-    continue;
   }
 
   return valid;
@@ -212,8 +163,32 @@ bool EntityCompiler::validate_all_component_definitions(xml_element_t *e) {
 }
 
 bool EntityCompiler::validate_a_component_definition(xml_element_t *e) {
-  // TODO(mtwilliams): Validate component definitions.
-  return false;
+  bool valid = true;
+
+  // Check for identifier.
+  if (e->num_of_attributes >= 1) {
+    const xml_fragment_t *name = &e->attributes[0].name,
+                         *value = &e->attributes[0].value;
+
+    if (core::string::compare("id", name->s, name->l)) {
+      if (!core::uuid::validate(value->s)) {
+        env_->error(env_, "Expected an identifier for component but got \"%.*s\" instead.", value->l, value->s);
+        valid = false;
+      }
+    } else {
+      env_->error(env_, "Expected an identifier for component but got \"%.*s\" instead.", name->l, name->s);
+      valid = false;
+    }
+  } else {
+    env_->error(env_, "Component not assigned an identifier. Will automatically be assigned one.");
+  }
+
+  // Ignore superfluous attributes.
+  if (e->num_of_attributes >= 2) {
+    env_->warning(env_, "Ignoring superfluous attributes on component definition.");
+  }
+
+  return valid;
 }
 
 bool EntityCompiler::validate_all_children_definitions(xml_element_t *e) {
